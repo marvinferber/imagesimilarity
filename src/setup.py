0a4bf11a8d50d849abf1be51d0c7ed591bb3d25c
Marvin Ferber <ferbhome@googlemail.com>
2020-07-18 21:01:24 +0200

    optimized setup.py for pyinstaller

--- src/setup.py
+++ src/setup.py
@@ -1,22 +1,44 @@
 from setuptools import setup
+import platform
 
-setup(
-    name='imagesimilarity',
-    version='0.1',
-    packages=[''],
-    url='https://github.com/marvinferber/imagesimilarity',
-    license='Apache License 2.0',
-    author='ferber',
-    author_email='ferbhome@freenet.de',
-    description='Find and display images that are similar.',
-    install_requires=[
-        'wxPython>=4',
-        'Pillow>=7',
-        'tensorflow==2.0.2',
-        'tensorflow_hub',
-        'annoy @ https://download.lfd.uci.edu/pythonlibs/w3jqiv8s/annoy-1.16.3-cp37-cp37m-win_amd64.whl',
-		'pywin32',
-		'setuptools<45',
-		'pyinstaller'
-    ]
-)
+if platform.system() == 'Windows':
+    setup(
+        name='imagesimilarity',
+        version='0.1',
+        packages=[''],
+        url='https://github.com/marvinferber/imagesimilarity',
+        license='Apache License 2.0',
+        author='ferber',
+        author_email='ferbhome@freenet.de',
+        description='Find and display images that are similar.',
+        install_requires=[
+            'wxPython>=4',
+            'Pillow>=7',
+            'tensorflow==2.0.2',
+            'tensorflow_hub',
+            'annoy @ https://download.lfd.uci.edu/pythonlibs/w3jqiv8s/annoy-1.16.3-cp37-cp37m-win_amd64.whl',
+		    'pywin32',
+		    'setuptools<45',
+		    'pyinstaller @ https://github.com/pyinstaller/pyinstaller/archive/develop.tar.gz'
+        ]
+    )
+else:
+    setup(
+        name='imagesimilarity',
+        version='0.1',
+        packages=[''],
+        url='https://github.com/marvinferber/imagesimilarity',
+        license='Apache License 2.0',
+        author='ferber',
+        author_email='ferbhome@freenet.de',
+        description='Find and display images that are similar.',
+        install_requires=[
+            'wxPython>=4',
+            'Pillow>=7',
+            'tensorflow==2.0.2',
+            'tensorflow_hub',
+            'annoy',
+		    'setuptools<45',
+		    'pyinstaller @ https://github.com/pyinstaller/pyinstaller/archive/develop.tar.gz'
+        ]
+    )
